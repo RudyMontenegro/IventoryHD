2ebe2ef6fc3553528975bebb423b1304a52c56f3
rolidoss <rudymontenegro4@gmail.com>
2021-04-23 09:57:00 -0400

    firts commit

---
+++ resources/views/auth/adm.blade.php
@@ -0,0 +1,78 @@
+@extends('plantilla')
+
+<head>
+    <link rel="shortcut icon" href="./images/logoempresa.png" type="image/x-icon" />
+</head>
+<nav class="navbar navbar-light" style="background-color: #FFCB00;" class="container">
+    <div class="row">
+        <div class="container-fluid">
+            <h1 style="color:#000">LISTA DE USUARIOS</h1>
+
+        </div>
+    </div>
+    </div>
+</nav>
+
+
+
+@section('seccion')
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VirtualTecnoDinamyc</title>
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg  bg-light">
+        <a href="{{ url('/') }}" class="btn btn-secondary btn-lg btn-block">Volver</a>
+    </nav>
+
+    <div class="container-fluid" style="background-color: #FFCB00 ">
+        <div>
+
+            <table class="table table-bordered table-responsive text-black">
+                <thead>
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Correo</th>
+                        <th scope="col">Acciones</th>
+
+                    </tr>
+                    @foreach($dis as $dis)
+                    <tr>
+                        <td>{{$dis->name}}</td>
+                        <td>{{$dis->email}}</td>
+                        <td>
+                           
+
+                        <a href="{{url('editar',$dis->id)}}" class="btn btn-success">Editar</a>
+                        
+                        
+                            <form action="{{url('showDisk/eliminar/'.$dis->id)}}" method="post" class="d-inline">
+                                {{csrf_field()}}
+                                {{ method_field('DELETE') }}
+                                <button class="btn btn-danger"
+                                    onclick=" return confirm ('¿Esta seguro de Eliminar este Usuario?'); "
+                                    type="submit">Eliminar</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+            </table>
+
+        </div>
+
+    </div>
+
+</body>
+<nav class="navbar navbar-expand-lg  bg-light">
+    <a href="{{ url('/') }}" class="btn btn-secondary btn-lg btn-block">Volver</a>
+</nav>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <title>VirtualTecnoDinamyc</title>
+    <link rel="shortcut icon" href="./images/logoempresa.png" type="image/x-icon" />
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="background-color: #0D6EFD">
+            <div class="container">
+                <div class="container-fluid">
+                    <h1 style="color:#fff">VIRTUAL TECNO DINAMYC</h1>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <!-- Authentication Links -->
+                            @if (Auth::guest())
+                            <div class="navbar-nav me-4">
+                                <div class="navbar-nav">
+                                   
+                                </div>
+
+                            </div>
+                        </div>
+                </div>
+
+
+                @else
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <ul class="dropdown-menu" role="menu">
+                        <li>
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+                @endif
+                </ul>
+            </div>
+    </div>
+    </nav>
+
+    @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
